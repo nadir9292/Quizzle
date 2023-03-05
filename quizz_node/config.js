94545a91c7540a8b822cb5f52afc9e228bcef2db
nadir9292 <nadirbtssio@gmail.com>
2023-03-05 03:04:54 +0100

    add vercel.json + update dockerfile

--- quizz_node/config.js
+++ quizz_node/config.js
@@ -4,6 +4,8 @@ export default {
   db: {
     client: process.env.DB_CLIENT,
     connection: {
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
       user: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_DATABASE,
--- quizz_node/readme.md
+++ quizz_node/readme.md
@@ -3,3 +3,9 @@
 - **Create a migration :** `npx knex migrate:make "nameExemple"`
 - **Run a migration :** `npx knex migrate:latest`
 - **Rollback the migration :** `npx knex migrate:rollback`
+
+## Step for docker : 
+```
+ - docker build -t quizzle .
+ - docker-compose -d up
+```
