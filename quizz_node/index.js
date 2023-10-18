d16a9df6a90d470f8ca61352974ed847450beac0
Nadir Mansouri <61685527+nadir9292@users.noreply.github.com>
2023-10-18 15:28:32 +0200

    add swagger

--- quizz_node/index.js
+++ quizz_node/index.js
@@ -1,9 +1,11 @@
 import express from "express"
-import knexfile from "./knexfile.js"
+import cors from "cors"
 import { Model } from "objection"
 import knex from "knex"
+import knexfile from "./knexfile.js"
 import allRoutes from "./src/routes/allRoutes.js"
-import cors from "cors"
+import swaggerJsDoc from "swagger-jsdoc"
+import swaggerUi from "swagger-ui-express"
 
 const app = express()
 app.use(
@@ -11,11 +13,28 @@ app.use(
     origin: "*",
   })
 )
-
 app.use(express.json())
+
 const db = knex(knexfile)
 Model.knex(db)
+
 const port = 3000
+
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Quizzle api",
+      version: "1.0.0",
+      description: "API documentation for quizzle api",
+    },
+  },
+  apis: ["./src/routes/*.js"],
+}
+
+const swaggerSpec = swaggerJsDoc(options)
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec))
 
 allRoutes({ app, db })
 
